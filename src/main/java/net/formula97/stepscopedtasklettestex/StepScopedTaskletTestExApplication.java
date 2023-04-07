fed8ce37942243ff0ccdf3948b461b33a8e1c96a
HAJIME Fukuna <f97one@gmail.com>
2023-04-08 08:48:25 +0900

    ひとまずプロジェクト生成直後を追加。

---
+++ src/main/java/net/formula97/stepscopedtasklettestex/StepScopedTaskletTestExApplication.java
@@ -0,0 +1,13 @@
+package net.formula97.stepscopedtasklettestex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepScopedTaskletTestExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StepScopedTaskletTestExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/formula97/stepscopedtasklettestex/StepScopedTaskletTestExApplicationTests.java
@@ -0,0 +1,13 @@
+package net.formula97.stepscopedtasklettestex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepScopedTaskletTestExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
